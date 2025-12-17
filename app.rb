964f69eea9ac12a3ede9dc65b2325c081c3798e0
Einar <einar.soderbergbeckman@elev.ga.ntig.se>
2025-12-17 20:56:41 +0100

    Add user authentication and per-user todo lists
    Introduces login functionality using sessions and a users table, requiring authentication to access and manage todos. Todos are now associated with an owner, and all todo queries are filtered by the logged-in user. The seeder and database schema are updated to support users and ownership. UI changes include a login form, category names from the database, and improved layout. Removes an unused image and updates CSS for layout improvements.

--- app.rb
+++ app.rb
@@ -2,66 +2,121 @@ require 'sinatra'
 require 'sqlite3'
 require 'slim'
 require 'sinatra/reloader'
+require 'sinatra/flash'
+# ^gem install sinatra-flash
+
+enable :sessions
 
 # Redirect from '/' to '/todos'
 get '/' do
-    redirect '/todos'  
+    slim :login
+end
+
+post '/login' do
+    usr = params[:username]
+    pwd = params[:password]
+    
+    db = SQLite3::Database.new("db/todos.db")
+    db.results_as_hash = true
+
+    result = db.execute("SELECT * FROM users")
+
+    result.each do |row|
+        if row["password"] == pwd and row["username"] == usr
+            @login = true
+        end
+    end
+
+    if @login
+        session[:user] = usr
+        redirect '/todos'
+    else
+        redirect '/'
+    end
 end
 
 # Index page
 get '/todos' do
+    #Uses sinatra-flash to show an alert message
+    if session[:user].nil?
+        flash[:notice] = "You must be logged in to add a task."
+        redirect '/'
+    end
+
     db = SQLite3::Database.new("db/todos.db")
     db.results_as_hash = true
 
     @categories = {}
-    @tasksUndone = db.execute("SELECT * FROM todos WHERE done = false")
-    @tasksDone = db.execute("SELECT * FROM todos WHERE done = true")
+    @tasksUndone = db.execute("SELECT * FROM todos WHERE done = false AND owner = ?", session[:user])
+    @tasksDone = db.execute("SELECT * FROM todos WHERE done = true AND owner = ?", session[:user])
 
     @tasksUndone.each do |item|
         @categories[item["id"]] = db.execute("SELECT name FROM categories INNER JOIN todos_categories_rel ON categories.id = todos_categories_rel.categories_id WHERE todos_categories_rel.todos_id = ?", item["id"]).map {|row| row["name"]}.join(", ")
     end
 
-    p @tasksUndone
-    p " "
-    p @categories
-
     @tasksDone.each do |item|
         @categories[item["id"]] = db.execute("SELECT name FROM categories INNER JOIN todos_categories_rel ON categories.id = todos_categories_rel.categories_id WHERE todos_categories_rel.todos_id = ?", item["id"]).map {|row| row["name"]}.join(", ")
     end
+
+    @categoryNames = {
+    1 => db.execute("SELECT name FROM categories WHERE id = 1")[0]["name"],
+    2 => db.execute("SELECT name FROM categories WHERE id = 2")[0]["name"],
+    3 => db.execute("SELECT name FROM categories WHERE id = 3")[0]["name"]
+    }
 
     slim :index
 end
 
 # Handles sorting by category
 get '/todos/:category' do
+    if session[:user].nil?
+        flash[:notice] = "You must be logged in to add a task."
+        redirect '/'
+    end
+
     db = SQLite3::Database.new("db/todos.db")
     db.results_as_hash = true
 
-    @category = params[:category]
+    @category = params[:category].to_i
 
     @categories = {}
-    @tasksUndone = db.execute("SELECT * FROM todos WHERE done = false")
-    @tasksDone = db.execute("SELECT * FROM todos WHERE done = true")
-
-    @tasksUndone.each do |item|
-        @categories[item["id"]] = db.execute("SELECT name FROM categories INNER JOIN todos_categories_rel ON categories.id = todos_categories_rel.categories_id WHERE todos_categories_rel.todos_id = ?", item["id"]).map {|row| row["name"]}.join(", ")
+    @tasksUndoneTemp = db.execute("SELECT * FROM todos WHERE done = false AND owner = ?", session[:user])
+    @tasksDoneTemp = db.execute("SELECT * FROM todos WHERE done = true AND owner = ?", session[:user])
+ 
+    @tasksUndoneTemp.each do |item|
+        @categories[item["id"]] = db.execute("SELECT categories.id FROM categories INNER JOIN todos_categories_rel ON categories.id = todos_categories_rel.categories_id WHERE todos_categories_rel.todos_id = ?", item["id"]).map { |row| row["id"] }
     end
 
-    whereContains = []
-    i = 0
+    @tasksDoneTemp.each do |item|
+        @categories[item["id"]] = db.execute("SELECT categories.id FROM categories INNER JOIN todos_categories_rel ON categories.id = todos_categories_rel.categories_id WHERE todos_categories_rel.todos_id = ?", item["id"]).map { |row| row["id"] }
+    end
 
-    @categories.each do |item|
-        if item[1].include?(@category)
-            whereContains << i
-        end
+    @tasksUndone = @tasksUndoneTemp.dup
+    @tasksDone = @tasksDoneTemp.dup
+
+    i = 0
+    while i < @tasksUndone.length
+        if !@categories[@tasksUndone[i]["id"]].include?(@category)
+            @tasksUndone.delete_at(i)
+            i -= 1
+        end  
         i += 1
     end
-    @sortedUndone = []
-    
 
-    @tasksDone.each do |item|
-        @categories[item["id"]] = db.execute("SELECT name FROM categories INNER JOIN todos_categories_rel ON categories.id = todos_categories_rel.categories_id WHERE todos_categories_rel.todos_id = ?", item["id"]).map {|row| row["name"]}.join(", ")
+    i = 0
+    while i < @tasksDone.length
+        if !@categories[@tasksDone[i]["id"]].include?(@category)
+            @tasksDone.delete_at(i)
+            i -= 1
+        end  
+        i += 1
     end
+
+    @categoryNames = {
+    1 => db.execute("SELECT name FROM categories WHERE id = 1")[0]["name"],
+    2 => db.execute("SELECT name FROM categories WHERE id = 2")[0]["name"],
+    3 => db.execute("SELECT name FROM categories WHERE id = 3")[0]["name"]
+    }
 
     slim :index
 end
@@ -74,16 +129,19 @@ post '/todos/new' do
     name = params[:name]
     description = params[:description]
     category = (params[:category])
-    category.map! {|item| item.to_i}
 
-    db.execute("INSERT INTO todos (name, description, done) VALUES (?,?,?)",[name,description,0])
+    db.execute("INSERT INTO todos (name, description, done, owner) VALUES (?,?,?,?)",[name,description, 0, session[:user]])
 
     idInserted = db.execute("SELECT last_insert_rowid()")[0]["last_insert_rowid()"]
+
+    if category.nil?
+        category = [3]
+    end
 
     category.each do |item|
         db.execute("INSERT INTO todos_categories_rel (todos_id, categories_id) VALUES (?,?)", [idInserted, item])
     end
-
+    
     redirect('/todos')
 end
 
@@ -94,13 +152,13 @@ post '/todos/:id/toggle' do
 
     id = params[:id].to_i
 
-    current = db.get_first_value("SELECT done FROM todos WHERE id = ?", id).to_i
+    current = db.execute("SELECT done FROM todos WHERE id = ?", id)[0]["done"].to_i
     new_state = current == 1 ? 0 : 1
 
     db.execute("UPDATE todos SET done = ? WHERE id = ?", [new_state, id])
 
     redirect('/todos')
-end
+end             
 
 # Deletes a task
 post '/todos/:id/delete' do
